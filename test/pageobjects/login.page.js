2ceeaf3e78cd6b172fbb035fad6c6fdaef07f786
Md. Mahbubur Rahman <mahbubasr091@gmail.com>
2023-03-25 19:29:41 +0600

    added simple test case

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -6,37 +6,42 @@ import Page from '../pageobjects/page.js';
  * sub page containing specific selectors and methods for a specific page
  */
 class LoginPage extends Page {
+
+        /**
+     * overwrite specific options to adapt it to page object
+     */
+        open () {
+            return super.open('login');
+        }
+
     /**
      * define selectors using getter methods
      */
-    get inputUsername () {
-        return $('#username');
-    }
-
-    get inputPassword () {
-        return $('#password');
+    get searchBar () {
+        return $("#entry_217820 [type='text']")
     }
 
     get btnSubmit () {
-        return $('button[type="submit"]');
+        return $("div#search  .type-text");
+    }
+
+    get myAccountButton() {
+        return $("div#widget-navbar-217834 > ul > li:nth-of-type(6) > a[role='button']  .title")
     }
 
     /**
      * a method to encapsule automation code to interact with the page
      * e.g. to login using username and password
      */
-    async login (username, password) {
-        await this.inputUsername.setValue(username);
-        await this.inputPassword.setValue(password);
+
+    async searchingForDesiredProduct (searchText) {
+        await this.searchBar.setValue(searchText);
         await this.btnSubmit.click();
     }
 
-    /**
-     * overwrite specific options to adapt it to page object
-     */
-    open () {
-        return super.open('login');
-    }
+
+
+
 }
 
 export default new LoginPage();
